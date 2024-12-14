925f73b0a373abeaf435f822c8cd68188f43d66f
Shinebayar Gansukh <3091558+shinebayar-g@users.noreply.github.com>
2024-12-13 18:04:20 -0800

    chore: update example

--- README.md
+++ README.md
@@ -1,3 +1,72 @@
 # k8skonf
 
 Kubernetes in Typescript. Like CDK8s / Helm but simpler.
+
+## Usage
+
+Source
+
+```ts
+import { K8sApp } from '@k8skonf/core';
+import { Deploymentv1 } from '@k8skonf/core/Deploymentv1';
+
+const app = new K8sApp('my-k8s-app');
+
+new Deploymentv1(app, 'my-nginx-deployment', {
+    spec: {
+        selector: {
+            matchLabels: {
+                app: 'nginx',
+            },
+        },
+        template: {
+            metadata: {
+                labels: {
+                    app: 'nginx',
+                },
+            },
+            spec: {
+                containers: [
+                    {
+                        name: 'nginx',
+                        image: 'nginx:alpine',
+                        ports: [
+                            {
+                                containerPort: 80,
+                                name: 'http',
+                            },
+                        ],
+                    },
+                ],
+            },
+        },
+    },
+});
+
+app.save();
+```
+
+Output
+
+```yaml
+---
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: my-nginx-deployment
+spec:
+  selector:
+    matchLabels:
+      app: nginx
+  template:
+    metadata:
+      labels:
+        app: nginx
+    spec:
+      containers:
+        - name: nginx
+          image: nginx:alpine
+          ports:
+            - containerPort: 80
+              name: http
+```
--- examples/src/main.ts
+++ examples/src/main.ts
@@ -1,48 +1,37 @@
 import { K8sApp } from '@k8skonf/core';
 import { Deploymentv1 } from '@k8skonf/core/Deploymentv1';
 
-function deployment(app: K8sApp) {
-    return new Deploymentv1(app, 'my-deployment', {
-        spec: {
-            selector: {
-                matchLabels: {
-                    foo: 'bar',
-                },
-            },
-            template: {
-                metadata: {
-                    labels: {
-                        foo: 'bar',
-                    },
-                },
-                spec: {
-                    containers: [
-                        {
-                            name: 'nginx',
-                            image: 'nginx:alpine',
-                            ports: [
-                                {
-                                    containerPort: 80,
-                                    name: 'http',
-                                },
-                            ],
-                        },
-                    ],
-                },
+const app = new K8sApp('my-k8s-app');
+
+new Deploymentv1(app, 'my-nginx-deployment', {
+    spec: {
+        selector: {
+            matchLabels: {
+                app: 'nginx',
             },
         },
-    });
-}
+        template: {
+            metadata: {
+                labels: {
+                    app: 'nginx',
+                },
+            },
+            spec: {
+                containers: [
+                    {
+                        name: 'nginx',
+                        image: 'nginx:alpine',
+                        ports: [
+                            {
+                                containerPort: 80,
+                                name: 'http',
+                            },
+                        ],
+                    },
+                ],
+            },
+        },
+    },
+});
 
-function main() {
-    const app = new K8sApp('my-k8s-app');
-    deployment(app);
-
-    const app2 = new K8sApp('my-k8s-app2', 'filePerResource');
-    deployment(app2);
-
-    app.save();
-    app2.save();
-}
-
-main();
+app.save();
